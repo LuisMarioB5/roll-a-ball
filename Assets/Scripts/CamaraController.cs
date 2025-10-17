5949ba84a73d8e2918b59bcda70eb2700001bfbb
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2025-10-17 15:42:12 -0400

    Se modifico el comportamiento del movimiento de la camara

---
+++ Assets/Scripts/CamaraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour {
+
+	// Referencia a nuestro jugador
+	public GameObject jugador;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		offset = transform.position - jugador.transform.position;
+	}
+	
+	// LateUpdate se llama después de que todos los Update hayan sido llamados
+	void LateUpdate()
+	{
+		transform.position = jugador.transform.position + offset;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
