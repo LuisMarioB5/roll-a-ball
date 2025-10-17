c6bd6bbc98ff0e78531f9a86e28a55e4261d81c9
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2025-10-17 12:46:14 -0400

    Se agrego movimiento al jugador

---
+++ Assets/Scripts/JugadorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorController : MonoBehaviour {
+
+	// Variable RigidBody para controlar el movimiento del jugador
+	private Rigidbody rb;
+
+	// Velocidad de movimiento del jugador
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Obtener entrada del usuario
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		// Crear vector de movimiento
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		// Aplicar fuerza al RigidBody para mover el jugador
+		rb.AddForce(movement * speed);
+	}																		
+}
